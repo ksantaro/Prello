2d536d44142bc97600feecdc7c8de5326018b063
ksantaro <ubkenneths@gmail.com>
2017-07-17 13:41:59 -0700

    Test

--- public/javascripts/reset-password.js
+++ public/javascripts/reset-password.js
@@ -4,16 +4,38 @@ var signUpForm = document.querySelector("#sign-up-form");
 var password = document.querySelector("#password");
 var confirmPassword = document.querySelector("#confirm-password");
 var signUpFormJ = $("#sign-up-form");
+var body = $("body");
 console.log(signUpForm);
 
 signUpForm.addEventListener("submit", function (e) {
   if(password.value !== confirmPassword.value) {
     e.preventDefault();
-    alert("The passwords are different");
+    var createModal = $("<div/>").attr("id", "email-no-small").addClass("modal-menu").append($("<div/>").addClass("add-new-board")
+      .append($("<h2/>").html("Passwords are different"))
+      .append($("<span/>").html("X")));
+    body.append(createModal);
   } else {
-    alert("success");
+    var createModal = $("<div/>").attr("id", "email-no-small").addClass("modal-menu").append($("<div/>").addClass("add-new-board")
+      .append($("<h2/>").html("Successful Change"))
+      .append($("<span/>").html("X")));
+    body.append(createModal);
+    setTimeout(function () {
+       window.location.href = "http://localhost:3000/login"; //will redirect to your blog page (an ex: blog.html)
+    }, 3500)
   }
 });
+
+//Close Modal If clicked elsewhere
+body.on("click",".modal-menu", function(e) {
+  if ($(this).is(e.target) && $(this).has(e.target).length === 0) {
+    $(this).remove();
+    $("body").css("overflow", "auto");
+}});
+//Close Modal
+body.on("click", ".modal-menu span", function(e) {
+  $(this).parent().parent().remove();
+});
+
 
 $(document).ready(function () {
 
